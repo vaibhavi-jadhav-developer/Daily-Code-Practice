b4083a9427aeecf4dedaab510e1fcaf6187ef07f
jvaibhavi363-a11y <jvaibhavi363@gmail.com>
2026-06-03 20:52:49 +0530

    added daily python and sql solutions

---
+++ Python/Arithmetic_operators.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum_num= a+b
+    diff_num= a-b
+    prod_num= a*b
+    print(sum_num)
+    print(diff_num)
+    print(prod_num)
+    
